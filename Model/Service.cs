b92b26ea39764e3ff32252b97651b1ba251fdd7d
Бальтазар Лолронов <Slavacer@bk.ru>
2022-02-01 16:17:59 +0300

    Добавьте файлы проекта.

---
+++ Model/Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Service
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        /// <summary>
+        /// In roubles
+        /// </summary>
+        public int Cost { get; set; }
+        /// <summary>
+        /// In seconds
+        /// </summary>
+        public int Duration { get; set; }
+        /// <summary>
+        /// In %
+        /// </summary>
+        public int Discount { get; set; }
+        public string MainImagePath { get; set; }
+    }
+}
---
+++ ViewModel/ViewModel1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class ViewModel1 : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Главный фрейм
+        /// </summary>
+        public Frame MainFrame { get; set; }
+        /// <summary>
+        /// Список страниц
+        /// </summary>
+        public List<Page> Pages { get; set; }
+        private bool _isAdmin = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Код авторизации. Если равен "0000" - авторизируется администратор
+        /// </summary>
+        public string Code 
+        { 
+            set
+            {
+                if(value == "0000")
+                {
+                    _isAdmin = true;
+                }
+            } 
+        }
+        /// <summary>
+        /// Переведёт на страницу с ServiceList в названии
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Authorisation(object sender, EventArgs e)
+        {
+            MainFrame.Content = Pages.Where(x => x.Name.Contains("ServiceList"));
+        }
+
+    }
+}
